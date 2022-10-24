b86ac7daf4ee34b7a785a1ccb6931ce0ebb93b66
Deimantas Smeledis <deimantas.smeledis@mediapark.com>
2022-10-24 13:58:25 +0300

    Video text recognition + tensorflow test

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from Service.VideoChecker import VideoChecker
+from Service.VideoTextRecognition import VideoTextRecognition
 
-videoChecker = VideoChecker()
-print(videoChecker.isLatestVideoNew(timeSearch="hours"))
+if __name__ == '__main__':
+    videoTextRecognition = VideoTextRecognition()
+    # videoTextRecognition.captureFrames("https://www.youtube.com/watch?v=2WrX8QMbXMQ")
+    videoTextRecognition.video_to_frames_url_auto("https://www.youtube.com/watch?v=2WrX8QMbXMQ")
+# videoChecker = VideoChecker()
+# if(videoChecker.isLatestVideoNew(timeSearch="hours") and videoChecker.videoIsLaughOrLose()):
+#     print("True")
+# else:
+#     print("false")
+    
